83e91977c99f2cf6a7da7be924f465d5a5d66350
germanvache <gvachet@yahoo.com.ar>
2025-07-16 21:24:04 -0300

    Initial commit gestión de supermercado en lenguaje java

---
+++ src/EsAlimento.java
@@ -0,0 +1,5 @@
+public interface EsAlimento {
+    boolean esAlimento();
+    int getCalorias();
+
+}
---
+++ src/ProductoLimpieza.java
@@ -0,0 +1,7 @@
+import java.time.LocalDate;
+
+public class ProductoLimpieza extends Producto{
+    public ProductoLimpieza(String nombre, double precio, LocalDate fechaCaducidad) {
+        super(nombre, precio, fechaCaducidad);
+    }
+}
